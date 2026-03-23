458da4255cbf587ce12f244f7ae11a55823f9215
shaches <69593611+shaches@users.noreply.github.com>
2026-03-23 12:48:39 +0300

    initial commit

---
+++ src/main/java/com/ideflux/testPlugin/TestPlugin.java
@@ -0,0 +1,20 @@
+package com.ideflux.testPlugin;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TestPlugin extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage(Component.text("Heeeello, my daaarling " + event.getPlayer().getName() + "!"));
+    }
+}
